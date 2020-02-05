4d15b0251f644e3c87d807fdde8b215559da027f
Gauri160 <gaurishankar453@gmail.com>
2020-02-05 03:58:54 +0000

    changes

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@ public class HelloWorld
 {
  public static void main (String args[])
  {
-  System.out.println("Hello Gauri Shankar ! Welcome on Git Hub PollSCM");
+  System.out.println("Hello Gauri Shankar ! Welcome on Git Hub PollSCM changes");
  }
 }
 
